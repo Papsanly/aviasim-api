6c7ede6299b3f255e4f995c921946013cd8ac403
Papsanly <papsanlysenko@gmail.com>
2024-03-11 07:41:09 +0200

    update schema

--- migrations/0_schema.sql
+++ migrations/0_schema.sql
@@ -1,12 +1,3 @@
-create table "client" (
-    id integer primary key generated always as identity,
-    phone varchar not null unique,
-    email varchar not null unique,
-    first_name varchar not null,
-    second_name varchar not null,
-    provider varchar not null
-);
-
 create table promocode (
     code varchar primary key,
     expire_date date not null,
@@ -16,9 +7,8 @@ create table promocode (
 
 create table "order_info" (
     id integer primary key generated always as identity,
-    client integer references "client" not null,
     duration smallint not null,
-    simulator_type varchar not null,
+    simulator varchar not null,
     payment_status varchar not null,
     promocode varchar references "promocode",
     comment text,
@@ -28,19 +18,36 @@ create table "order_info" (
 
 create table "direct_order" (
     order_info integer primary key references "order_info",
+    client_phone varchar not null,
+    client_email varchar not null,
+    client_name varchar not null,
+    client_provider varchar not null,
     time timestamp not null
 );
 
+-- separate table for gift_code as there are physical pre-generated codes 
 create table "gift_code" (
     code varchar(4) primary key,
+    -- expire date could always be the end of the year 
     expire_date date not null,
     is_physical boolean not null
 );
 
 create table "gift_order" (
     order_info integer primary key references "order_info",
+    -- if its a physical certificate use random value from gift_code
+    -- if not generate a new one
     gift_code varchar(4) references "gift_code" not null unique,
-    recipient integer references "client" not null,
+    recipient_phone varchar not null,
+    recipient_email varchar not null,
+    recipient_name varchar not null,
+    -- if null - certificate is physical
     delivery_address varchar,
-    is_activated boolean not null
+    client_phone varchar not null,
+    client_email varchar not null,
+    client_name varchar not null,
+    -- client orders a gift - time is set to null for now
+    -- then recipient activates the certificate - time is set to the value
+    -- that recipient choose
+    time timestamp
 );
